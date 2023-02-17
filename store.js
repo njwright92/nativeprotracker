7dd987078468612822d0e7e075d9916b07b51d8f
njwright92 <njwright92@gmail.com>
2023-02-17 12:42:28 -0800

    "added middleware logger"

--- store.js
+++ store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
