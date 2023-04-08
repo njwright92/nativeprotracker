e065862dd6dac59ebc72acebb4a4e74cc99a54a5
njwright92 <njwright92@gmail.com>
2023-04-08 11:02:09 -0700

    "delete"

--- actions/deleteEntry.js
+++ actions/deleteEntry.js
@@ -1,10 +1,18 @@
 import { DELETE_ENTRY } from './types';
+import { deleteDoc, doc } from 'firebase/firestore';
+import { firestore } from '../firebaseConfig';
 
 export const deleteEntry = (itemId, entryId) => {
-    return (dispatch) => {
-        dispatch({
-            type: DELETE_ENTRY,
-            payload: { itemId, entryId }
-        });
+    return async (dispatch) => {
+        try {
+            const entryRef = doc(firestore, 'items', itemId, 'entries', entryId);
+            await deleteDoc(entryRef);
+            dispatch({
+                type: DELETE_ENTRY,
+                payload: { itemId, entryId },
+            });
+        } catch (error) {
+            console.error('Error deleting entry: ', error);
+        }
     };
 };
