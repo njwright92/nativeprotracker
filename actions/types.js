658df66d2688bdc5dad3862bbe1cff187f80defa
njwright92 <njwright92@gmail.com>
2023-05-06 10:56:16 -0700

    Updates

--- actions/types.js
+++
@@ -1,6 +0,0 @@
-export const ADD_ITEM = 'ADD_ITEM';
-export const UPDATE_ITEM = 'UPDATE_ITEM';
-export const DELETE_ITEM = 'DELETE_ITEM';
-export const ADD_ENTRY = 'ADD_ENTRY';
-export const DELETE_ENTRY = 'DELETE_ENTRY';
-export const EDIT_ENTRY = 'EDIT_ENTRY';
--- reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import itemsReducer from './items';
-
-const rootReducer = combineReducers({
-    items: itemsReducer,
-});
-
-export default rootReducer;
