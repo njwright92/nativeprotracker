951e56cd51a2702f6b07f580ddfc3e4a731f44b4
njwright92 <njwright92@gmail.com>
2023-11-17 13:59:56 -0800

    "conditionally rendered landing page"

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState, useEffect } from "react";
 import { ActivityIndicator } from "react-native";
 import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
@@ -7,16 +7,35 @@ import Main from "./screens/MainComponent";
 import LoginScreen from "./screens/LoginScreen";
 import store from "./store";
 import { createStackNavigator } from "@react-navigation/stack";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const Stack = createStackNavigator();
+const auth = getAuth();
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true); // Add a loading state
+
+  useEffect(() => {
+    // Listen for authentication state changes
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setIsLoggedIn(!!user);
+      setLoading(false); // Set loading to false when auth state is determined
+    });
+
+    return unsubscribe;
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator />; // Show loading indicator while waiting for auth state
+  }
+
   return (
     <Provider store={store}>
       <NavigationContainer>
         <Suspense fallback={<ActivityIndicator />}>
           <Stack.Navigator
-            initialRouteName="Landing"
+            initialRouteName={isLoggedIn ? "Main" : "Landing"}
             screenOptions={{ headerShown: false }}
           >
             <Stack.Screen name="Landing" component={LandingPage} />
